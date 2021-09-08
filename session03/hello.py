81d503dc5bd7dfe50ea4bde7fd8719b639219eb0
michael4227 <zsun1@babson.edu>
2021-09-08 19:57:04 -0400

    Create hello.py

---
+++ session03/hello.py
@@ -0,0 +1,18 @@
+print('tell me your name')
+name = input()  # input('Please enter your name >>>')
+print('hello', name, 'this is your computer')
+
+year_of_birth = int(input('PLease enter your year of birth'))
+# year_of_birth = int(year_of_birth)
+print('Your year of birth is', year_of_birth, ';age is', 2021-year_of_birth)
+
+print('nanannanananna. \n' * 10)
+pi = 3.141592653589
+print(f'Pi equals {pi:5f}')
+
+# pseudocode
+# 1. ask user to enter the year of birth, expecting user to enter an integer
+# 2. "store" the integer to a variable, year; convert the variable to an integar
+# 3. calculate the age, 2021 - year
+# 4. "store" the result to another variable
+# 5. print the age
