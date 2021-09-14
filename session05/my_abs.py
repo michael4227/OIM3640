576469381278a8a562aca1fd69879cb9f486573c
michael4227 <zsun1@babson.edu>
2021-09-14 19:20:47 -0400

    q

---
+++ session05/my_abs.py
@@ -0,0 +1,4 @@
+def my_abs(a):
+    my_abs = abs(a)
+    return my_abs
+print(my_abs(-999))
---
+++ session05/session05.py
@@ -0,0 +1,15 @@
+#functions
+import random, math
+print(random.random())
+print(int(random.random()*10))
+math.floor(-1.5)
+random.randint(0,10)
+
+def print_A(r):
+    """print the area of circle with given radius"""
+    A=3.14*r*r
+    print(A)
+
+r=60
+r=2
+print_A(r)
