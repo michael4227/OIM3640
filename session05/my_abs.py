9a9cbe6465459e9cbd9ee74029795f3dcbddff46
michael4227 <zsun1@babson.edu>
2021-09-14 20:42:41 -0400

    f

--- session05/my_abs.py
+++ session05/my_abs.py
@@ -1,4 +1,22 @@
-def my_abs(a):
-    my_abs = abs(a)
-    return my_abs
-print(my_abs(-999))
+# def my_abs(a):
+#     if a > 0:
+#         print(a)
+#     else:
+#         print(-a)
+# my_abs(-99)
+
+# def my_abs2(a):
+#     if a > 0:
+#         return a
+#     else:
+#         return -a
+# print (my_abs2(-99))
+
+def my_abs2(a):
+    if a > 0:
+        print(a)
+    else:
+        print(-a)
+    return a, -a
+# my_abs2(-99)
+print(123)
