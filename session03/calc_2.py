caa034726dc67853736274c81005b69ff53bbdd1
michael4227 <zsun1@babson.edu>
2021-09-18 17:31:07 -0400

    Update calc_2.py

--- session03/calc_2.py
+++ session03/calc_2.py
@@ -1,16 +1,16 @@
-# 1
-pi = 3.1415926
-r = 5
-volumn = (4/3)*pi*r**3
-print(volumn)
+# # 1
+# pi = 3.1415926
+# r = 5
+# volumn = (4/3)*pi*r**3
+# print(volumn)
 
-# 2
-cp = 24.95
-dc = 1-0.4
-unit = 60
-sc = 3 + 0.75*(unit-1)
-total_cost = cp*unit*dc - sc
-print(total_cost)
+# # 2
+# cp = 24.95
+# dc = 1-0.4
+# unit = 60
+# sc = 3 + 0.75*(unit-1)
+# total_cost = cp*unit*dc - sc
+# print(total_cost)
 
 # 3
 # calculate time needed in seconds
@@ -20,7 +20,12 @@ time3 = time1
 # calculate total time in minutes
 tot_time = (time1+time2+time3)/60
 print(tot_time)
-print('the time arrived home is', 6+1, ':', (tot_time+52)-60, 'am')
+time = 6+1
+print(f'the time arrived home is {time} : {tot_time+52-60}am')
+
+#print('the time arrived home is', 6+1, ':', (tot_time+52)-60, 'am')
 
 # 4
-print('{:.1f}'.format((89-82)/82*100), '%')
+var = '{:.2f}'.format((89-82)/82*100)
+print(f'{var}%')
+
