f938fd50ac4743a21ef95ea856554dfa5ef24979
michael4227 <zsun1@babson.edu>
2021-09-21 18:29:21 -0400

    session 7

---
+++ session05/a.py
@@ -0,0 +1,15 @@
+def my_abs(a):
+    if a > 0:
+        print(a)
+    else:
+        print(-a)
+    return "michael"
+    print(my_abs(-123))
+
+def addition(a, b):
+    result = a + b 
+    print(result)
+    #return result
+    #return result
+    return result
+print(addition(3,5) + 6)
