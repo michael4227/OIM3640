9268d401358eaffd05e21ecfcc17881a1b6e1e20
michael4227 <zsun1@babson.edu>
2021-10-06 23:02:35 -0400

    10.6

--- session10/try.py
+++ session10/try.py
@@ -5,3 +5,18 @@ for letter in prefixes:
         print(letter + 'u' + suffix)
     else:
         print(letter + suffix)
+
+name = 'zanjiesunmichael'
+count = 0
+for i in name:
+    if i == 'a' or i =='e' or i =='i' or i =='o' or i =='u':
+        count += 1
+print(count)    
+
+
+def count_letter(name, letter):
+    count = 0
+    for name in letter:
+        count +=1
+    return count
+print(count_letter('michael', 'iae'))
---
+++ session11/word.py
@@ -0,0 +1,206 @@
+import os
+print(os.getcwd())
+
+
+# Assume words.txt is under data folder
+# f = open('data/words.txt')
+# line = f.readline()
+# print(line.strip())
+
+
+# f = open('data/words.txt')
+
+# number_of_words = 0
+
+# for line in f:
+#     word = line.strip()
+#     number_of_words += 1
+
+# print(number_of_words)
+
+
+# def find_long_words():
+#     """
+#     prints only the words with more than 20 characters
+#     """
+#     f = open('data/words.txt')  # Assume words.txt is under data folder
+
+#     for line in f:
+#         word = line.strip()
+#         if len(word) > 20:
+#             print(word, len(word))
+
+
+# find_long_words()
+
+
+def has_no_e(word):
+    """
+    returns True if the given word doesn’t have the letter "e" in it
+    """
+    for letter in word:
+        if letter.lower() == 'e':
+            return False
+    return True
+
+# print(has_no_e('Babson'))
+# print(has_no_e('College'))
+# print(has_no_e('EA sports'))
+
+
+def find_words_no_e():
+    """
+    returns the percentage of the words that don't have the letter "e"
+    """
+    f = open('data/words.txt')  # Assume words.txt is under data folder
+    total_words = 0
+    words_no_e = 0
+    for line in f:
+        word = line.strip()
+        if has_no_e(word):
+            # print(word)
+            words_no_e += 1
+        total_words += 1
+    
+    return total_words, total_words
+
+a, b = find_words_no_e()
+perc_no_e  = a / b
+print(f'The percentage of the words with no "e" is {perc_no_e*100:.2f}%.')
+
+
+def avoids(word, forbidden):
+    """
+    returns True if the given word does not use any of the forbidden letters
+    """
+    f = open('data/words.txt')  # Assume words.txt is under data folder
+    for line in f:
+        word = line.strip()
+        for letter in word:
+            if letter in forbidden:
+                return False
+        return True
+# modify
+def avoids(word, forbidden):
+    """
+    print if the given word does not use any of the forbidden letters
+    """
+    f = open('data/words.txt')  # Assume words.txt is under data folder
+    for line in f:
+        word = line.strip()
+        for letter in word:
+            if letter == forbidden:
+                continue
+            else: 
+                print(word)
+
+print(avoids('Babson', 'abcde'))
+# print(avoids('College', 'e'))
+# print(avoids('Boston', 'xyz'))
+
+
+def find_words_no_vowels():
+    """
+    returns the percentage of the words that don't have vowel letters
+    """
+    f = open('data/words.txt')
+    for line in f:
+        word = line.strip()
+        for letter in word:
+            if letter == 'aeiou':
+                continue
+        print(word)
+
+
+perc_no_vowel = find_words_no_vowels()
+# print(f'The percentage of the words without vowel letters is {perc_no_vowel*100:.2f}%.')
+
+
+def uses_only(word, available):
+    """
+    takes a word and a string of letters, and that returns True if the word
+    contains only letters in the string available.
+    """
+    pass
+
+
+# print(uses_only('Babson', 'aBbsonxyz'))
+# print(uses_only('college', 'aBbsonxyz'))
+
+
+def find_words_only_use_planet():
+    """only containing planet"""
+    f = open('data/words.txt')
+    for line in f:
+        word = line.strip()
+        for letter in word:
+            if letter not in 'planet':
+                continue
+        print(word)
+
+
+print('Number of words that use only letters from "planets" is', find_words_only_use_planet())
+
+
+def uses_all(word, required):
+    """
+    takes a word and a string of required letters, and that returns True if
+    the word uses all the required letters at least once.
+    """
+    pass
+
+
+# please write test cases
+
+
+def find_words_using_all_vowels():
+    """
+    return the number of the words that use all the vowel letters
+    """
+    pass
+
+
+# print('The number of words that use all the vowels:', find_words_using_all_vowels())
+
+
+def is_abecedarian(word):
+    """
+    returns True if the letters in a word appear in alphabetical order
+    (double letters are ok).
+    """
+    pass
+
+
+# print(is_abecedarian('abs'))
+# print(is_abecedarian('college'))
+
+
+def find_abecedarian_words():
+    """
+    returns the number of abecedarian words
+    """
+    pass
+
+
+# print(find_abecedarian_words())
+
+
+def is_abecedarian_using_recursion(word):
+    """
+    returns True if the letters in a word appear in alphabetical order
+    (double letters are ok).
+    """
+    pass
+
+
+# print(is_abecedarian_using_recursion('abcdef'))
+
+
+def is_abecedarian_using_while(word):
+    """
+    returns True if the letters in a word appear in alphabetical order
+    (double letters are ok).
+    """
+    pass
+
+# print(is_abecedarian_using_while('abcdef'))
