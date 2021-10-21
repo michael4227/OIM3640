93b03c759872247babf2f86cf03243d782f472f4
michael4227 <zsun1@babson.edu>
2021-10-21 00:43:01 -0400

    /
    /

---
+++ session13/json_example.py
@@ -0,0 +1,18 @@
+# api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+import urllib.request
+import json
+
+APIKEY = 'ce93208cd4f103313fb5e02486627eb8'
+city = 'Wellesley'
+country_code = 'us'
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&APPID={APIKEY}'
+
+# print(url)
+f = urllib.request.urlopen(url)
+response_text = f.read().decode('utf-8')
+response_data = json.loads(response_text)
+print(response_data)
+
+# How do we get current temperature?
+response_data['main']
