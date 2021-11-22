4e01a655d277dc11ef7f18e14c3d95dfa98e7de1
michael4227 <zsun1@babson.edu>
2021-11-22 14:30:34 -0500

    /
    /

--- OOP/OOP1/Circle1_solution.py
+++ OOP/OOP1/Circle1_solution.py
@@ -1,25 +1,12 @@
-from OOP1.Point1 import *
+from OOP1.Point1_solution import *
 import copy
-box = Rectangle()
-box.width = 100.0
-box.height = 200.0
-box.corner = Point()
-box.corner.x = 50.0
-box.corner.y = 50.0
-p = copy.copy(box.corner)
+
 
 class Circle:
     """Represents a circle.
 
     Attributes: center, radius
     """
-circle1 = Circle()
-circle1.center = Point()
-circle1.center.x = 20
-circle1.center.y = 40
-circle1.radius = 75
-
-print(circle1.radius)
 
 
 def point_in_circle(point, circle):
@@ -29,6 +16,7 @@ def point_in_circle(point, circle):
     circle: Circle object
     """
     d = distance_between_points(point, circle.center)
+    # print(d)
     return d <= circle.radius
 
 
@@ -39,7 +27,7 @@ def rect_in_circle(rect, circle):
     circle: Circle object
     """
     p = copy.copy(rect.corner)
-    print_point(p)
+    # print_point(p)
     if not point_in_circle(p, circle):
         return False
 
@@ -67,32 +55,53 @@ def rect_circle_overlap(rect, circle):
     rect: Rectangle object
     circle: Circle object
     """
+    p = copy.copy(rect.corner)
+    # print_point(p)
+    if point_in_circle(p, circle):
+        return True
+
+    p.x += rect.width
+    # print_point(p)
+    if point_in_circle(p, circle):
+        return True
+
+    p.y += rect.height
+    # print_point(p)
+    if point_in_circle(p, circle):
+        return True
+
+    p.x -= rect.width
+    # print_point(p)
+    if point_in_circle(p, circle):
+        return True
+
+    return False
 
 
 def main():
     box = Rectangle()
-    # box.width = 100.0
-    # box.height = 200.0
-    # box.corner = Point()
-    # box.corner.x = 50.0
-    # box.corner.y = 50.0
+    box.width = 100.0
+    box.height = 200.0
+    box.corner = Point()
+    box.corner.x = 50.0
+    box.corner.y = 50.0
 
-    # print(box.corner.x)
-    # print(box.corner.y)
+    print(box.corner.x)
+    print(box.corner.y)
 
-    # circle = Circle
-    # circle.center = Point()
-    # circle.center.x = 150.0
-    # circle.center.y = 100.0
-    # circle.radius = 75.0
+    circle = Circle
+    circle.center = Point()
+    circle.center.x = 150.0
+    circle.center.y = 100.0
+    circle.radius = 75.0
 
-    # print(circle.center.x)
-    # print(circle.center.y)
-    # print(circle.radius)
+    print(circle.center.x)
+    print(circle.center.y)
+    print(circle.radius)
 
-    # print(point_in_circle(box.corner, circle))
-    # print(rect_in_circle(box, circle))
-    # print(rect_circle_overlap(box, circle))
+    print(point_in_circle(box.corner, circle))
+    print(rect_in_circle(box, circle))
+    print(rect_circle_overlap(box, circle))
 
 
 if __name__ == '__main__':
--- OOP/OOP1/Point1_solution.py
+++ OOP/OOP1/Point1_solution.py
@@ -1,25 +1,32 @@
 import math
+
+
 class Point:
     """Represents a point in 2-D space.
 
     attributes: x, y
     """
+
+
+# my_point = Point()
+# # print(my_point)
+# # print(type(my_point))
+# # print(my_point.__doc__)
+# my_point.x = 3.0
+# my_point.y = 4.0
+# # print(my_point.x, my_point.y)
+
+
 def print_point(p):
-    print(f'({p.x},{p.y})')
-    
-# mich = Point()
-# print(mich)
-# print(type(mich))
-# print(mich.__doc__)
-# mich.x = 2
-# mich.y = 3
-# print(mich.x, mich.y)
+    """Print a Point object in human-readable format."""
+    print(f"({p.x}, {p.y}).")
 
-# print_point(mich)
 
-# print(hasattr(mich, 'x'))
-# print(hasattr(mich, 'z'))
-# print(dir(mich))
+# print_point(my_point)
+
+# print(hasattr(my_point, 'x'))
+# print(hasattr(my_point, 'z'))
+# print(dir(my_point))
 
 
 def distance_between_points(p1, p2):
@@ -37,13 +44,14 @@ def distance_between_points(p1, p2):
 
 
 # another_point = Point()
-# another_point.x = 6
-# another_point.y = 8
-# another_point.x, another_point.y = 6, 8
+# # another_point.x = 6
+# # another_point.y = 8
 
-# print_point(mich)
+# another_point.x, another_point.y = 6, 8
+# print()
+# print_point(my_point)
 # print_point(another_point)
-# print(distance_between_points(mich, another_point))
+# print(distance_between_points(my_point, another_point))
 
 
 class Rectangle:
@@ -52,49 +60,52 @@ class Rectangle:
     attributes: width, height, corner.
     """
 
-# print(distance_between_points(mich, box.corner))
-box = Rectangle()
-box.width = 100.0
-box.height = 200.0
-box.corner = Point()
-box.corner.x = 30
-box.corner.y = 40
 
-def find_center(box):
+# box = Rectangle()
+# box.width = 100.0
+# box.height = 200.0
+# box.corner = Point()
+# box.corner.x = 0
+# box.corner.y = 0
+
+# print(distance_between_points(my_point, box.corner))
+
+
+def find_center(rect):
     """Returns a Point at the center of a Rectangle.
 
     rect: Rectangle
 
     returns: new Point
     """
-    center = Point()
-    center.x = box.corner.x + box.width / 2
-    center.y = box.corner.y + box.height / 2
-    return center
+    p = Point()
+    p.x = rect.corner.x + rect.width / 2.0
+    p.y = rect.corner.y + rect.height / 2.0
+    return p
 
 
 # center_of_box = find_center(box)
 # print_point(center_of_box)
 
 
-def grow_rectangle(box, cw, ch):
+def grow_rectangle(rect, dwidth, dheight):
     """Modifies the Rectangle by adding to its width and height.
 
     rect: Rectangle object.
     dwidth: change in width (can be negative).
     dheight: change in height (can be negative).
     """
-    box.width += cw
-    box.height += ch
+    rect.width += dwidth
+    rect.height += dheight
 
 
-def print_rectangle(box):
+def print_rectangle(rect):
     """
     prints the width, height and lower-left corner of the given Rectangle object
     """
-    print(f"width: {box.width}, height:{box.height}")
+    print(f"width: {rect.width}, height:{rect.height}")
     print("the lower-left corner:")
-    print_point(box.corner)
+    print_point(rect.corner)
 
 
 # print_rectangle(box)
@@ -112,7 +123,8 @@ def main():
     print_point(my_point)
 
     print('Is my_point an instance of Point?', isinstance(my_point, Point))
-    print('Is my_point an instance of Rectangle?',isinstance(my_point, Rectangle))
+    print('Is my_point an instance of Rectangle?',
+          isinstance(my_point, Rectangle))
     print('Does my_point have an attribute x?', hasattr(my_point, 'x'))
     print('Does my_point have an attribute z?', hasattr(my_point, 'z'))
 
@@ -120,19 +132,46 @@ def main():
     box.width = 100.0
     box.height = 200.0
     box.corner = Point()
-    box.corner.x = 70
-    box.corner.y = 80
+    box.corner.x = 0.0
+    box.corner.y = 0.0
 
     print('Does box have an attribute x?', hasattr(box, 'x'))
+
     print('Does box have an attribute corner?', hasattr(box, 'corner'))
+
     print('Rectangle has these:', dir(box))
 
     center = find_center(box)
+    print('center', end=' ')
     print_point(center)
+
+    # print(box.width)
+    # print(box.height)
     print_rectangle(box)
+    print('grow')
     grow_rectangle(box, 50, 100)
+    # print(box.width)
+    # print(box.height)
     print_rectangle(box)
 
 
 if __name__ == "__main__":
     main()
+    # p1 = Point()
+    # p1.x = 3
+    # p1.y = 4
+
+    # import copy
+    # p2 = copy.copy(p1)
+
+    # print_point(p1)
+    # print_point(p2)
+
+    # print(id(p1))
+    # print(id(p2))
+    # print(p1 is p2)
+
+    # print(p1.x is p2.x)
+    # print(p1 == p2)
+
+    # print(dir(p1))
